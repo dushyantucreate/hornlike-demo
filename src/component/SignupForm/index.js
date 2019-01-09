6d110de49256e36ef70be7fab85a406c44f26660
Sahil Dhiman <sahil@ucreate.co.in>
2019-01-09 15:45:21 +0530

    implemented the validation on signup form

--- src/component/SignupForm/index.js
+++ src/component/SignupForm/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Button } from 'semantic-ui-react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-
+import * as Yup from 'yup';
+import { isEmpty } from 'lodash';
 import Header from '../TopHeader/TopHeader';
 
 const Basic = () => (
@@ -11,66 +12,71 @@ const Basic = () => (
           <div className="sign-up-container">          
             <h1>Sign up for Free</h1>
             <Formik
-                initialValues={{companyname:'', desiredurl: '', email: '', password: '' }}
-                validate={values => {
-                let errors = {};
-                if (!values.companyname) {
-                    errors.companyname = 'Enter the company name';
+                initialValues={{companyname:'', desiredurl: '', email: '', password: '', confirmpassword: '' }}
+                validationSchema = {
+                    Yup.object().shape({
+                        companyname: Yup.string().required('This field is required.'),
+                        desiredurl: Yup.string().required('This field is required.'),
+                        email: Yup.string()
+                          .required('This field is required.')
+                          .email('Invalid email address.'),
+                        password: Yup.string().required('This field is required'),
+                        confirmpassword: Yup.string()
+                          .oneOf([Yup.ref('password'), null], 'Passwords must match')
+                    })
                 }
-                else if (!values.desiredurl) {
-                    errors.desiredurl = 'Enter the company url';
-                }
-                else if (!values.email) {
-                    errors.email = 'Required';
-                } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                ) {
-                    errors.email = 'Invalid email address';
-                }
-                return errors;
-                }}
                 onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
-                }, 400);
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
                 }}
-                >
-                {({ isSubmitting }) => (
-                <Form className="signup-form">
-                    <h3>Company Details</h3>
+                render={({
+                    dirty,
+                    touched,
+                    errors,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting,
+                  }) => (
+                    <div className="login-row">
+                        <div className="signup-form">
+                          <Form onSubmit={handleSubmit}>
+                            <h3>Company Details</h3>
 
-                    <label>Company Name</label>
-                    <Field type="text" name="companyname" />
-                    <ErrorMessage className="error" name="companyname" component="div" />
+                            <label>Company Name</label>
+                            <Field type="text" name="companyname" />
+                            <ErrorMessage className="error" name="companyname" component="div" />
 
-                    <label>Desired URL</label>    
-                    <Field type="text" name="desiredurl" />
-                    <ErrorMessage className="error" name="desiredurl" component="div" />
+                            <label>Desired URL</label>    
+                            <Field type="text" name="desiredurl" />
+                            <ErrorMessage className="error" name="desiredurl" component="div" />
 
 
-                    <h3>Account Details</h3>
-                    <label>Email</label>
-                    <Field type="email" name="email" />
-                    <ErrorMessage className="error" name="email" component="div" />
+                            <h3>Account Details</h3>
+                            <label>Email</label>
+                            <Field type="email" name="email" />
+                            <ErrorMessage className="error" name="email" component="div" />
 
-                    <label>Password</label>
-                    <Field type="password" name="password" />
-                    <ErrorMessage className="error" name="password" component="div" />
+                            <label>Password</label>
+                            <Field type="password" name="password" />
+                            <ErrorMessage className="error" name="password" component="div" />
 
-                    <label>Confirm Password</label>
-                    <Field type="password" name="confirmpassword" />
-                    <ErrorMessage className="error" name="confirmpassword" component="div" />                     
+                            <label>Confirm Password</label>
+                            <Field type="password" name="confirmpassword" />
+                            <ErrorMessage className="error" name="confirmpassword" component="div" />                     
 
-                    <button className="signup-btn" type="submit" disabled={isSubmitting}>
-                    Sign Up
-                    </button>
-                </Form>
-                )}
-            </Formik>
-            </div>  
-      </Container>
-    </div>
-  );
+                            <Button className="signup-btn" type="submit"  disabled={isSubmitting || !isEmpty(errors) || !dirty}>
+                                Sign Up
+                            </Button>
+                          </Form>
+                        </div>
+                    </div>
+               )}
+            />
+        </div>
+    </Container>
+</div>);
   
-  export default Basic;
+export default Basic;
