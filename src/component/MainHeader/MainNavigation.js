36b1f176650607ae361f74f3ad42090a069a6863
Sahil Dhiman <sahil@ucreate.co.in>
2019-01-16 12:28:47 +0530

    created the component for landing page

--- src/component/MainHeader/MainNavigation.js
+++ src/component/MainHeader/MainNavigation.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import { Dropdown, Image } from 'semantic-ui-react';
-import UserIcon from '../../images/profile_image.jpg';
+import { Input, Menu } from 'semantic-ui-react'
 
 
+export default class UserNavigation extends Component {
+  state = { activeItem: 'home' }
 
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
-class UserNavigation extends Component {
   render() {
-      return ( 
-        <div>
-          navigtion          
-        </div>        
-      );
+    const { activeItem } = this.state
+
+    return (
+      <Menu secondary>
+        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+        <Menu.Item
+          name='Feature'
+          active={activeItem === 'Feature'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='Pricing'
+          active={activeItem === 'Pricing'}
+          onClick={this.handleItemClick}
+        />        
+        <Menu.Item
+          name='Contactus'
+          active={activeItem === 'Contactus'}
+          onClick={this.handleItemClick}
+        />        
+        <Menu.Item
+          name='Login'
+          active={activeItem === 'Login'}
+          onClick={this.handleItemClick}
+        />                
+        <Menu.Item
+          name='Register'
+          active={activeItem === 'LogRegisterin'}
+          onClick={this.handleItemClick}
+        />                        
+      </Menu>
+    )
   }
 }
-  
-export default UserNavigation;
