4d4c0a09e9fcdc681b8a0b1ec9bbbfafb06f240c
Sahil Dhiman <sahil@ucreate.co.in>
2019-02-04 12:53:04 +0530

    updated the hero section content

--- src/component/HeroSection/Hero.js
+++ src/component/HeroSection/Hero.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { Grid, Button} from 'semantic-ui-react'
 
 
 class Hero extends Component {
   render() {
       return ( 
-          <div>
-            <section className="hero-section">
-                <h1 className="hero-title">Change the way you manage time-offtrack job applicants</h1>
-                <h5 className="hero-content">SnapHRM is a modern HR Management Software built with speed and ease of use in mind. We are trusted by thousands of companies worldwide.</h5>
-            </section>
-          </div>
+        <section className="hero-section">
+            <Grid columns='equal' divided inverted padded>
+                <Grid.Row textAlign='center'>
+                    <Grid.Column width={10} className='hero-content-section'>                        
+                        <h1 className="hero-title">Change the way you manage time-offtrack job applicants</h1>
+                        <h5 className="hero-content">SnapHRM is a modern HR Management Software built with speed and ease of use in mind. We are trusted by thousands of companies worldwide.</h5>                    
+                        <Button className="btn-style" color='green' size='massive'>Login</Button>
+                        <Button className="btn-style" color='grey' size='massive'>Signup</Button>                        
+                    </Grid.Column> 
+                </Grid.Row>
+            </Grid>
+          </section>
       );
   }
 }
